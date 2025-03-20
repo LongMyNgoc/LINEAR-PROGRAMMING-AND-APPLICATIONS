f9843c6358608ba1fd87e80793a529f666c0d7d8
LongMyNgoc <longmyngoc2004@gmail.com>
2025-03-20 20:42:20 +0700

    Insert Link PPT

--- src/components/Courses/QuyHoachTuyenTinh/QuyHoachTuyenTinhContent/QuyHoachTuyenTinhContent.ts
+++ src/components/Courses/QuyHoachTuyenTinh/QuyHoachTuyenTinhContent/QuyHoachTuyenTinhContent.ts
@@ -5,16 +5,16 @@ export const chapters = [
         key: "1",
         contents: [
             { title: "Đề cương chi tiết môn học", link: "/documents/Gioithieu/Decuong_COMP1502.pdf" },
-            { title: "Video giới thiệu", link: "https://www.youtube.com/watch?v=example2" },
+            { title: "Video giới thiệu", link: "https://www.youtube.com/watch?v=llqopwIWSTQ" },
         ]
     },
     {
         title: "Chương 1: Bài toán quy về quy hoạch tuyến tính",
         key: "2",
         contents: [
-            { title: "1.1 Slide bài giảng chương 1 (Buổi 1)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/ET0TDffrg4pLkEfTojIckb8ByXMS96XtHslcApmgbOygEg?e=N6UJr9" },
+            { title: "1.1 Slide bài giảng chương 1 (Buổi 1)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EZtR0-j63M1Hp9M8FExKnAoBLJgcPsx5TsLU0I1zwi5ilA?e=6y6bmp" },
             { title: "1.2 Bài giảng chương 1 (Buổi 1)", link: "https://www.youtube.com/watch?v=example2" },
-            { title: "1.3 Slide bài giảng chương 1 (Buổi 2)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EYrpYgGH9EFIpsOZ6bVB1MQBzmrpkQfj0hC6ryl4iM3vEw?e=xXJnx2" },
+            { title: "1.3 Slide bài giảng chương 1 (Buổi 2)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EVw9SJql4eNLpfGg_AxBImkBdnfVtk3K-Wf765vVLoh_3g?e=HsfdF1" },
             { title: "1.4 Bài giảng chương 1 (Buổi 2)", link: "https://www.youtube.com/watch?v=example3" },
             { title: "1.5 Bài tập chương 1 (Tự Luận)", link: "https://studenthcmupedu-my.sharepoint.com/:b:/g/personal/4801104082_student_hcmue_edu_vn/EfVp6wd0aQxGied15-sskg0B8nTA7HzYqFbJlyFJp-7V3g?e=3JorRa" },
             { title: "1.6 Bài tập chương 1 (Trắc Nghiệm)", link: "https://www.youtube.com/watch?v=example3" }
@@ -24,11 +24,11 @@ export const chapters = [
         title: "Chương 2: Thuật toán đơn hình",
         key: "3",
         contents: [
-            { title: "2.1 Slide bài giảng thuật toán đơn hình (1)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EUl0KWZ-co9Hu7SSuEdpe_MBvMw4YmUAoemYxevPx-dtKQ?e=BvyF8Q" },
+            { title: "2.1 Slide bài giảng thuật toán đơn hình (1)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/ERT1i-kBP7ZLtlgF0bCsngsBV9QxizUrzNK1_i0sERPRpw?e=T3pgk1" },
             { title: "2.2 Bài giảng thuật toán đơn hình (1)", link: "https://www.youtube.com/watch?v=example4" },
-            { title: "2.3 Slide bài giảng thuật toán đơn hình (2)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EcBx4lxqwy1Gj97FN_fmVvcBlQl7G5H8wZIyBiBvUgdNWQ?e=dEhkMD" },
+            { title: "2.3 Slide bài giảng thuật toán đơn hình (2)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EbBQawGzb4FNvSY7vnXH3yMBYcmXQF6dPc15naQCqXtjqg?e=m4Jq8k" },
             { title: "2.4 Bài giảng thuật toán đơn hình (2)", link: "https://www.youtube.com/watch?v=example4" },
-            { title: "2.5 Slide bài giảng thuật toán đơn hình (3)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EVMeVUQC2MJAiJh9sSSpFIYBgvX3pYC1ID84ZismHJE7MQ?e=rN3k29" },
+            { title: "2.5 Slide bài giảng thuật toán đơn hình (3)", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/ETUTaQHfhUlIgZVCG9OUHWYBp6MHU0mYaqp_Osx3wPdfvQ?e=dT7JZw" },
             { title: "2.6 Bài giảng thuật toán đơn hình (3)", link: "https://www.youtube.com/watch?v=example4" },
             { title: "2.7 Bài tập chương 2 (Tự Luận)", link: "https://studenthcmupedu-my.sharepoint.com/:b:/g/personal/4801104082_student_hcmue_edu_vn/Efa0ZlYKa8FDsH5d1DxQAwIBTGEwsXZwg7BsSRtiakDFnw?e=fOfMlM" },
             { title: "2.8 Bài tập chương 2 (Trắc Nghiệm)", link: "https://www.youtube.com/watch?v=example5" }
@@ -38,7 +38,7 @@ export const chapters = [
         title: "Chương 3: Bài toán đối ngẫu",
         key: "4",
         contents: [
-            { title: "3.1 Slide bài toán đối ngẫu", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EUbMcwxuOXtDodBac-I6NkQB8CDgitLhlEa1CzCVoaoMSA?e=zbdMUH" },
+            { title: "3.1 Slide bài toán đối ngẫu", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EQYAk9tz5H9BoA4-nRbllQ8B7SZLsC56DsTfheV6PtgMzQ?e=8MgWyD" },
             { title: "3.2 Bài giảng bài toán đối ngẫu", link: "https://www.youtube.com/watch?v=example7" },
             { title: "3.3 Bài tập chương 3 (Tự Luận)", link: "https://studenthcmupedu-my.sharepoint.com/:b:/g/personal/4801104082_student_hcmue_edu_vn/EYK1uoejVeBBu1gEYtIh_z4BjICipUn1Ka7lhNlt4xRrVA?e=bRtOVa" },
             { title: "3.4 Bài tập chương 3 (Trắc Nghiệm)", link: "https://www.youtube.com/watch?v=example6" }
@@ -48,7 +48,7 @@ export const chapters = [
         title: "Chương 4: Bài toán vận tải",
         key: "5",
         contents: [
-            { title: "4.1 Slide bài giảng bài toán vận tải", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/ET4kce7jHzZOn5YupBxgkSYBdJ_-Z3ZRr7BV1XHqwk0dBA?e=2fgfum" },
+            { title: "4.1 Slide bài giảng bài toán vận tải", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/ESPTlvAb0B9Kqms1b6bYeWMBD65d84mxvxyAlsbLolAzRw?e=yfy5Lv" },
             { title: "4.2 Bài giảng bài toán vận tải", link: "https://www.youtube.com/watch?v=example8" },
             { title: "4.3 Bài tập chương 4 (Tự Luận)", link: "https://studenthcmupedu-my.sharepoint.com/:b:/g/personal/4801104082_student_hcmue_edu_vn/EZHBMaPs7qhGvp4VgpDdlT8B_Ti--pSnhlolM-jzc1DVvA?e=5IinQR" },
             { title: "4.4 Bài tập chương 4 (Trắc Nghiệm)", link: "https://www.youtube.com/watch?v=example9" }
@@ -58,7 +58,7 @@ export const chapters = [
         title: "Chương 5: Sơ đồ mạng",
         key: "6",
         contents: [
-            { title: "5.1 Slide bài giảng sơ đồ mạng", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EctH3kf91WtCn-UI5X0eFXcB00MuF2Ao_kB_E_MQRn8ZFA?e=Ih3YYt" },
+            { title: "5.1 Slide bài giảng sơ đồ mạng", link: "https://studenthcmupedu-my.sharepoint.com/:p:/g/personal/4801104082_student_hcmue_edu_vn/EUlE3FqaS6FHoN-06GmjrhgBZzFiVX0ffPBy2uklt4PyQg?e=RvWylO" },
             { title: "5.2 Bài giảng sơ đồ mạng", link: "https://www.youtube.com/watch?v=example10" },
             { title: "5.3 Bài tập chương 5 (Tự Luận)", link: "https://studenthcmupedu-my.sharepoint.com/:b:/g/personal/4801104082_student_hcmue_edu_vn/ESRmvwMF0zdFtl8ve8_NO5kBWNQRUmk6mEvQQl7z-hZg4g?e=AQ7cBa" },
             { title: "5.4 Bài tập chương 5 (Trắc Nghiệm)", link: "https://www.youtube.com/watch?v=example11" }
