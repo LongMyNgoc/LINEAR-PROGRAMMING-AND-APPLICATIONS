3f41ce3e8ea62d8b037a960736405d7176a9c076
LongMyNgoc <longmyngoc2004@gmail.com>
2025-03-24 16:55:44 +0700

    Insert Tool

--- src/components/Student/Navbar/Sidebar/Sidebar.tsx
+++ src/components/Student/Navbar/Sidebar/Sidebar.tsx
@@ -3,9 +3,13 @@ import Logo from '../../../../assets/Student/Logo.png';
 //import DashBoard from '../../../../assets/Student/DashBoard.png';
 import Profile from '../../../../assets/Student/Profile.png';
 import Courses from '../../../../assets/Student/Courses.png';
+import Tool from '../../../../assets/Student/Tool.png'
 //import Exams from '../../../../assets/Student/Exams.png';
 
 const Sidebar = ({ setActiveTab }: { setActiveTab: (tab: string | null) => void }) => {
+    const handleToolClick = () => {
+        window.open('https://nckh-qhtt-six.vercel.app/', '_blank', 'noopener,noreferrer');
+    };
     return (
         <nav className="sidebar">
             <div className="sidebar-logo">
@@ -25,6 +29,9 @@ const Sidebar = ({ setActiveTab }: { setActiveTab: (tab: string | null) => void 
                     setActiveTab('courses');
                 }}>
                     <img src={Courses} alt="Courses" className="courses-icon" />
+                </li>
+                <li onClick={handleToolClick}>
+                    <img src={Tool} alt="Tool" className='tool-icon' />
                 </li>
                 {/*<li onClick={() => setActiveTab('exams')}>
                     <img src={Exams} alt="Exams" className="exams-icon" />
